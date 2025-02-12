743c1dea87f44f044d3925400b3f273a2a4866a6
shohan12727 <aishohan001@gmail.com>
2025-02-12 12:04:23 +0600

    js related concept

---
+++ looping-with-array.js
@@ -0,0 +1,10 @@
+const richmans = ["elon","musa","hasina","kader"];
+// for (const richman of richmans){
+//     // console.log (richman);
+// } 
+
+for (let i = 0 ; i <10 ; i++){
+    console.log (i);
+    console.log(richmans[i]);
+}
+
