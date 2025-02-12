ec468f88d52f3179a51ce8897fd286eda169a56f
shohan12727 <aishohan001@gmail.com>
2025-02-12 12:25:52 +0600

    reverse added

--- reverse.js
+++ reverse.js
@@ -3,3 +3,9 @@ numbers.reverse();
 console.log(numbers);
 // here the array will run in a reverse
 
+const rollNumber = [1,2,3,8,65,2,21,36,412,21,54,32,365,25];
+for(i=rollNumber.length-1;i<=0;i--){
+    const num = rollNumber[i];
+    console.log(num);
+}
+
