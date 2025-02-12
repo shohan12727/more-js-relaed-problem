e36f433424f9deb52b240d06f388b3f5fa34add2
shohan12727 <aishohan001@gmail.com>
2025-02-12 13:15:08 +0600

    updated

---
+++ sort.js
@@ -0,0 +1,7 @@
+const names = ["shohan","aimanto","xakib","zaju"];
+const sorted_names = names.sort();
+console.log(sorted_names);
+
+// assending samller to larger
+// decending larger to smaller 
+
